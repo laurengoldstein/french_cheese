902a863aac1bce6d332c54f9978a76bb83ff3b7d
Lauren Goldstein <laurengolds@gmail.com>
2022-12-18 17:14:55 +0100

    Sequelize init and added users table to db

---
+++ api/migrations/20221218151843-create-user.js
@@ -0,0 +1,38 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        field: "first_name",
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        field: "last_name",
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("users");
+  },
+};
