866ef51e441e32cd8de96573267d6ed9101e334f
Lauren Goldstein <laurengolds@gmail.com>
2022-12-27 17:05:58 +0100

    recreated the db

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import "./Home.css";
 
 function HomeView(props) {
@@ -7,9 +8,16 @@ function HomeView(props) {
       <h1>Terroir</h1>
       <h2>Discover the regional cheeses of France.</h2>
       <h3>
-        Search by département, city, and/or milk type to uncover all that
-        France's creameries have to offer.
+        The Terroir app is designed with cheese-lovers in mind. Search by
+        département, city, and/or milk type to uncover all that France's
+        creameries have to offer. Save cheeses you'd like to try, and add the
+        most delicious ones to your favorites list. Connect with fellow cheese
+        enthusiasts to get recommendations and more.
       </h3>
+      <h4>Login or register to unlock all the features of Terroir.</h4>
+      <Link className="btn btn-outline-success" type="button" to="/login">
+        Login/Register
+      </Link>
     </div>
   );
 }
