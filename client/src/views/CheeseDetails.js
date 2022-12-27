514be91c04be8efd927b90fac60230b6923ef3b6
Lauren Goldstein <laurengolds@gmail.com>
2022-12-27 16:24:13 +0100

    added Bootstrap icons, formatted cheeseDetails page

--- client/src/views/CheeseDetails.js
+++ client/src/views/CheeseDetails.js
@@ -4,8 +4,57 @@ import React from "react";
 function CheeseDetailsView(props) {
   return (
     <div className="CheeseDetailsView">
-      <h1>Cheese Name</h1>
-      <h2>Info</h2>
+      <header>
+        {/* <button
+        key={c.id}
+        id="button"
+        type="button"
+        className="btn btn-outline-danger savedbtn py-0 mb-4"
+        name={c.name}
+        onClick={(e) => toggleSaved(c)}
+      >
+        {c.saved ? (
+          <i className="bi bi-plus-circle-fill"></i>
+        ) : (
+          <i className="bi bi-plus-circle"></i>
+        )}
+      </button>
+      <button
+        key={c.id}
+        id="button"
+        type="button"
+        className="btn btn-outline-danger favoritebtn py-0 mb-4"
+        name={c.name}
+        onClick={(e) => toggleFavorite(c)}
+      >
+        {c.favorite ? (
+          <i className="bi bi-heart-fill"></i>
+        ) : (
+          <i className="bi bi-heart"></i>
+        )}
+      </button> */}
+      </header>
+
+      <div className="row">
+        <div className="col-3">
+          <img />
+        </div>
+        <div className="col-9">
+          <h1>Cheese Name</h1>
+
+          <h3>Départment</h3>
+          <h3>Milk Type</h3>
+          <h3>Overall Rating</h3>
+        </div>
+      </div>
+      <div className="row">
+        <h3>Description</h3>
+        <p></p>
+      </div>
+
+      <div>
+        <h2>Users who love this cheese</h2>
+      </div>
     </div>
   );
 }
--- client/src/views/CheeseSearch.js
+++ client/src/views/CheeseSearch.js
@@ -4,7 +4,7 @@ import React from "react";
 function CheeseSearchView(props) {
   return (
     <div className="CheeseSearchView">
-      <h1>Find your new favorite French cheese!</h1>
+      <h1>Find your new favorite French cheeses!</h1>
       <h2>
         Search by département, city and/or milk type to uncover all that
         France's creameries have to offer.
